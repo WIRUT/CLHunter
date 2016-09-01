717feb07ddb187ff888a07b03f1c1785a0f9443a
Ross K <rossk@sfu.ca>
2016-09-01 01:02:41 -0700

    Making sure stuff has been committed

--- tests/test_clhousehunter.py
+++ tests/test_clhousehunter.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 def load_src(name, fpath):
     import os, imp
     return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
@@ -32,23 +33,35 @@ class TestInAreaOfInterest(unittest.TestCase):
 
     def test_in_aoi(self):
         aoi = [49.26938671, -123.1148529053]
-        self.assertEqual(clhousehunter.in_area_of_interest(aoi),(True,'False_Creek'))
+        self.assertEqual(clhousehunter.in_area_of_interest(aoi),
+                (True,'False_Creek'))
 
     def test_empty_aoi(self):
         aoi = []
-        self.assertEqual(clhousehunter.in_area_of_interest(aoi),(False, None))
+        self.assertEqual(clhousehunter.in_area_of_interest(aoi),
+                (False, None))
 
-# class TestInAOIMethods(unittest.TestCase):
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-    #
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #                 s.split(2)
+
+class TestCLInfoParser(unittest.TestCase):
+    area = "Coal Harbour"
+    info = {'price': '$9999', 'name': 'Waterfront Penthouse for rent!', 
+            'url': 'http://www.vancitylofts.com'} 
+    desc = "$9999 | Waterfront Penthouse for rent! | Coal Harbour "\
+             "| <http://www.vancitylofts.com>"
+    desc_mia_area = "$9999 | Waterfront Penthouse for rent! | N/A "\
+             "| <http://www.vancitylofts.com>"
+
+    def test_cl_info_parser(self):
+        self.assertEqual(clhousehunter.cl_info_parser(self.area, self.info), 
+                self.desc)
+
+    def test_cl_info_parser_one_empty(self):
+        self.assertEqual(clhousehunter.cl_info_parser(self.area, None), None)
+        self.assertEqual(clhousehunter.cl_info_parser(None, self.info), 
+                self.desc_mia_area)
+
+    def test_cl_info_parser_empty(self):
+        self.assertEqual(clhousehunter.cl_info_parser(None, None), None)
 
 if __name__ == '__main__':
     unittest.main()
