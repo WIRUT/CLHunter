76f287401a74c9b5932124fccd18bb31a99bc75a
Ross K <rossk@sfu.ca>
2016-09-03 23:36:10 -0700

    Added completion message to bash script

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ wait
 echo "pip install git+https://github.com/juliomalegria/python-craigslist"
 wait
 echo "pip install slackclient sqlalchemy python-dateutil"
+wait
+echo "Installation complete. To run CLHunter type: 'python lib/main.py'"
